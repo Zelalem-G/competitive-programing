2f8e0097b7175776ec9eb5c1ed6d1aa851764119
Zelalem <zelalemg935@gmail.com>
2026-03-05 10:38:41 -0800

    Add solution for Sort Characters By Frequency (trial 1)

---
+++ leetcode/sort-characters-by-frequency_trial1.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution(object):
+    def frequencySort(self, s):
+        freq = Counter(s)
+        ans  = ""
+
+        sorted_freq = sorted(freq.items(), key=lambda item:item[1],reverse=True)
+
+        for c,v in sorted_freq:
+            for i in range(v,0,-1):
+                ans += c
+
+        return ans
