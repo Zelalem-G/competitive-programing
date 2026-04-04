a2913b37cb9d92d1ace2da7bfac6d5b16c3b2410
Zelalem <zelalemg935@gmail.com>
2026-04-04 13:36:02 -0700

    Add solution for Reverse Linked List II (trial 1)

---
+++ leetcode/reverse-linked-list-ii_trial1.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
+
+        cur = prev.next
+
+        for _ in range(right - left):
+            temp = cur.next
+            cur.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
