4553046b2d6992270682a54f11c45091ec11ddc0
Zelalem <zelalemg935@gmail.com>
2026-04-22 11:02:31 -0700

    Add solution for Asteroid Collision (trial 1)

---
+++ leetcode/asteroid-collision_trial1.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        stack = []
+
+        for a in asteroids:
+            while stack and stack[-1] > 0 and a < 0:
+                if abs(a) > stack[-1]:
+                    stack.pop()
+                    continue
+                elif abs(a) == stack[-1]:
+                    stack.pop()
+                break
+            else:
+                stack.append(a)
+
+        return stack
