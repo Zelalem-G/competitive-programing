d902c90994feddb2a28edace6ddd10e457454cb3
Zelalem <zelalemg935@gmail.com>
2026-04-29 12:01:26 -0700

    Time: 4 ms (43.87%) | Memory: 16.4 MB (82.41%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -3,19 +3,9 @@ class Solution(object):
         if not root:
             return TreeNode(val)
 
-        cur = root
-        while cur:
-            if val < cur.val:
-                if cur.left:
-                    cur = cur.left
-                else:
-                    cur.left = TreeNode(val)
-                    break
-            else:
-                if cur.right:
-                    cur = cur.right
-                else:
-                    cur.right = TreeNode(val)
-                    break
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
 
         return root
