cf9cd2a74ca1691adec78b6c83cf4dc7c0f8ab13
Zelalem <zelalemg935@gmail.com>
2026-05-03 11:59:43 -0700

    Time: 118 ms (96.04%) | Memory: 15 MB (93.81%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def smallestDivisor(self, nums, threshold):
+        
+        def can(d):
+            total = 0
+            for n in nums:
+                total += (n + d - 1) // d
+            return total <= threshold
+
+        l, r = 1, max(nums)
+
+        while l < r:
+            m = (l + r) // 2
+
+            if can(m):
+                r = m
+            else:
+                l = m + 1
+
+        return l
